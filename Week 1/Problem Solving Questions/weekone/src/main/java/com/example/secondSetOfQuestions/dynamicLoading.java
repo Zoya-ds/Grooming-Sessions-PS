547ea65480ee5b8995f8157435a2a91db8b6bf7f
Zoya Shams <zoya.shams@publicissapient.com>
2023-03-24 16:05:18 +0530

    dynamic loading

--- Week 1/Problem Solving Questions/weekone/src/main/java/com/example/secondSetOfQuestions/dynamicLoading.java
+++ Week 1/Problem Solving Questions/weekone/src/main/java/com/example/secondSetOfQuestions/dynamicLoading.java
@@ -5,12 +5,13 @@ import java.lang.reflect.InvocationTargetException;
 public class dynamicLoading {
 
     public static void main(String[] args)
-            throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+            throws ClassNotFoundException, InstantiationException, IllegalAccessException, IllegalArgumentException,
+            InvocationTargetException, NoSuchMethodException, SecurityException {
 
         Class cl = Class.forName("com.example.secondSetOfQuestions.test");
 
         System.out.println(cl.getName());
-        Object obj = cl.newInstance();
+        Object obj = cl.getDeclaredConstructor().newInstance();
 
         test test1 = (test) obj;
         test1.display("Greetings!");
